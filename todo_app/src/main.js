81e0b636b55ff9edae1e1c351c832fff5ab61ab6
helloworld753315 <helloworld.cpp.h@gmail.com>
2021-06-12 16:22:33 +0900

    firebaseの認証機能追加

--- todo_app/src/main.js
+++ todo_app/src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyBGuDQnMoJkWrgXHLyZOd4y57vfqWKVho0',
+  authDomain: 'todo-e3407.firebaseapp.com',
+  projectId: 'todo-e3407',
+  storageBucket: 'todo-e3407.appspot.com',
+  messagingSenderId: '50675655522',
+  appId: '1:50675655522:web:f6197dea7e441bd80f023b'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
